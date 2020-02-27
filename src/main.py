fd220f62005d59a61954dbca64bbbc22ef179172
angelogzz <angelogzz87@gmail.com>
2020-02-27 17:47:09 -0600

    [Update] Se encuentra el texto oculto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 # Resolve the problem!!
-
+import os
+import re
 
 def run():
     # Start coding here
-
+    with open(os.getcwd() + '/src/encoded.txt', 'r', encoding='utf-8') as f:
+        encoded_text = f.read()
+        secret = re.findall("[a-z]",encoded_text)
+        print (''.join(secret))
 
 if __name__ == '__main__':
     run()
